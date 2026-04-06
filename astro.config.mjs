5567d707bd39348a9df26bd77d5d0917610d5383
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2026-04-06 11:28:01 -0700

    Initial commit: Astro static clone of emeasuretool.cms.gov

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://www.emeasuretool.cms.gov',
+  build: {
+    assets: 'assets',
+  },
+});
